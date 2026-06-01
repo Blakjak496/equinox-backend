5a908aca52539c6126131402b697d6ebe38db8eb
Will Farnell-Smith <willfs496@gmail.com>
2026-06-01 20:01:12 +0100

    added missing dotenv load to register-commands

--- src/bot/register-commands.ts
+++ src/bot/register-commands.ts
@@ -1,4 +1,7 @@
 import { REST, Routes, SlashCommandBuilder } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const applicationId = process.env.DISCORD_APPLICATION_ID;
 const guildId = process.env.DISCORD_GUILD_ID;
